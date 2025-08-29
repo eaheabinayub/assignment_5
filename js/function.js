3a3d8daf2320c8bfcc628babf82d000aa3434807
eaheabinayub <eahea.sbmc403@gmail.com>
2025-08-29 06:01:28 +0600

    call buttons functionality added

--- js/function.js
+++ js/function.js
@@ -14,3 +14,18 @@ for (btn of heartButtons) {
 let coins = 100;
 const coinCount = document.getElementById("coinCount");
 const callButtons = document.querySelectorAll(".call-btn");
+for (btn of callButtons) {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    const card = e.currentTarget.closest(".bg-white");
+    const serviceName = card.querySelector(".service-name").innerText;
+    const serviceNumber = card.querySelector(".service-number").innerText;
+    if (coins < 20) {
+      alert("You don’t have enough coins! (At least 20 are required)");
+      return;
+    }
+    alert(`Calling ${serviceName} - ${serviceNumber}`);
+    coins -= 20;
+    coinCount.innerText = coins;
+  });
+}
